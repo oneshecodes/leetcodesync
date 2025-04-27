8b8c9cd24f1f4578b1a8a4c13e6e0480ea8125bd
oneshecodes <vanshiprivate@gmail.com>
2025-04-27 18:51:05 +0530

    Time: 23 ms (5.15%) | Memory: 21.1 MB (5.24%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
+    vector<int> topoSortBFS(int numCourses, unordered_map<int, list<int>> &adjList) {
+        unordered_map<int, int> indegree;
+        vector<int> ans;
+
+        for (int i = 0; i < numCourses; i++) {
+            indegree[i] = 0;
+        }
+
+        for (auto it1 : adjList) {
+            for (auto it2 : it1.second) {
+                indegree[it2]++;
+            }
+        }
+
+        queue<int> q;
+
+        for (auto it : indegree) {
+            if (it.second == 0) {
+                q.push(it.first);
+            }
+        }
+
+        while(!q.empty()) {
+            int frontNode = q.front();
+            ans.push_back(frontNode);
+            q.pop();
+
+            for (auto nbr : adjList[frontNode]) {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+
+        if (ans.size() == numCourses)
+            return ans;
+        return {};
+    }
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        // Step 1: Create adjacency list for the graph
-        vector<vector<int>> adj(numCourses);
-		for (int i=0;i<prerequisites.size();i++) {
-            // prerequisites[i][0] depends on prerequisites[i][1]
-			adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-		}
+        unordered_map<int, list<int>> adjList;
 
-        // Step 2: Compute in-degree (number of incoming edges) for each node
-		vector<int> indegree(numCourses,0);
-		for (int i = 0; i < numCourses; i++) {
-			for (auto it : adj[i]) {
-				indegree[it]++;
-			}
-		}
-
-        // Step 3: Initialize queue with all nodes having 0 in-degree
-		queue<int> q;
-		for (int i = 0; i < numCourses; i++) {
-			if (indegree[i] == 0) {
-				q.push(i);
-			}
-		}
-
-        // Step 4: Perform BFS (Kahn's Algorithm for Topological Sort)
-		vector<int> ans; // to store the topological ordering
-		while (!q.empty()) { 
-			int node = q.front();
-			q.pop();
-			ans.push_back(node);
-			
-            // Decrease in-degree of adjacent nodes
-			for (auto it : adj[node]) {
-				indegree[it]--;
-                // If in-degree becomes 0, add to queue
-				if (indegree[it] == 0) q.push(it);
-			}
-		}
-
-        // Step 5: If all nodes are included in proper ordering, just return it
-		if (ans.size() == numCourses) return ans;
-
-    // Cycle detected or prerequisites is not in proper ordering     
-	return {};   
+        for (auto it : prerequisites) {
+            adjList[it[1]].push_back(it[0]);
+        }
+        return topoSortBFS(numCourses, adjList);
     }
 };
